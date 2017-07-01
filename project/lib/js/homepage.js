ef41744babdaca7edf4426887051b83f65beef18
cjjared007 <jared.cjrb@gmail.com>
2017-07-02 00:26:19 +0800

    added hovers

--- project/lib/js/homepage.js
+++ project/lib/js/homepage.js
@@ -1,8 +1,18 @@
 var homeBar = document.querySelector(".landSearch");
 homeBar.addEventListener("focus", function(){
-	//this.className += " searchClicked";
 	homeBar.classList.toggle("searchClicked");
-})
+});
 homeBar.addEventListener("blur", function(){
 	homeBar.classList.toggle("searchClicked");
-})
+});
+
+var menuOpen = document.querySelectorAll(".redirect");
+
+for(i=0; i<menuOpen.length; i++){
+menuOpen[i].addEventListener("mouseover", function(){
+	this.classList.toggle("onHover");
+});
+menuOpen[i].addEventListener("mouseleave", function(){
+	this.classList.toggle("onHover");
+});
+}
